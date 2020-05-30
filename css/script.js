1aea593755d11b062fb983932065a18d309b71c6
AbdiKkp <64970008+AbdiKkp@users.noreply.github.com>
2020-05-30 12:25:04 +0200

    c

--- css/script.js
+++ css/script.js
@@ -3,8 +3,9 @@ $(document).ready(function() {
    $('.js--section-features').waypoint(function(direction){
        
       if (direction == "down") {
-       $('nav').addClass('sticky');    
-      } else {
+       $('nav').addClass('sticky'); 
+     }       
+     else {
          $('nav').removeClass('sticky')
       }
    })
