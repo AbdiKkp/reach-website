f3fd3bc9897c71da55f148fb34eae8bca6956a8a
AbdiKkp <64970008+AbdiKkp@users.noreply.github.com>
2020-05-31 15:13:59 +0200

    Update script.js

--- css/script.js
+++ css/script.js
@@ -13,6 +13,12 @@ $(document).ready(function() {
      $('.js--scroll-to-plans').click(function () {
           
           $('html, body').animate({scrollTop: $('.js--section-plans').offset().top},1000);
+     }); 
+     
+     
+     $('.js--scroll-to-starts').click(function () {
+          
+          $('html, body').animate({scrollTop: $('.js--section-features').offset().top},1000);
      });
                  
 });
